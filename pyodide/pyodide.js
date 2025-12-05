deba5e58019f9593d77d1d0cafa9b3a3c913c70f
thordnel <thordnel@gmail.com>
2025-12-05 13:33:46 +0800

    offline pydide

---
+++ pyodide/pyodide.js
@@ -0,0 +1,14 @@
+<html>
+<head><title>404 Not Found</title></head>
+<body>
+<h1>404 Not Found</h1>
+<ul>
+<li>Code: NoSuchKey</li>
+<li>Message: The specified key does not exist.</li>
+<li>Key: v0.21.03/full/pyodide.js</li>
+<li>RequestId: XDFMQN5RHA30XB3F</li>
+<li>HostId: 5XjrReod7qGI410kFaoJanQICS00GpBQyW1yeO4hc3U5Bvzpm7KWk0AuyUC5VGwSD6KfnrQg19WfvZzbpWHOGOFJrQJbnur/</li>
+</ul>
+<hr/>
+</body>
+</html>
