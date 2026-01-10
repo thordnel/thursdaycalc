e75bc4214297eb36dfa94cf95f5e650f51fe46d5
thordnel <thordnel@gmail.com>
2026-01-10 21:48:37 +0800

    year filter, and pwa setup

---
+++ sw.js
@@ -0,0 +1,29 @@
+// /Users/td.buringon/Documents/Github/eclipsecalc/sw.js
+const CACHE_NAME = 'thursday-finder-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './pyscript.css',
+  './pyodide/pyodide.js',
+  './pyodide/pyodide.asm.js',
+  './pyodide/pyodide.asm.wasm',
+  './pyodide/pyodide.asm.data',
+  './pyodide/repodata.json',
+  './pyodide/pyodide_py.tar'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
